f87de3b8b933335f3dac63a42ce5c6ffd7ccb55d
Levi Richardson <litbelb1@outlook.com>
2021-02-18 21:59:46 -0800

    add sudo, change apt-get to apt

--- best-practices.sh
+++ best-practices.sh
@@ -1,6 +1,6 @@
-apt-get install sudo -y
-sudo apt-get update && sudo apt-get upgrade -y
-sudo apt autoremove
-useradd newuser
-usermod -aG sudo newuser
-reboot
+sudo apt install sudo -y
+sudo apt update && sudo apt-get upgrade -y
+sudo apt autoremove -y
+sudo useradd newuser
+sudo usermod -aG sudo newuser
+sudo reboot
